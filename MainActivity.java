1e70c398d61e558e5b6e94382be98c713d762b68
ROHIT <rohitmaurya100@gmail.com>
2018-04-07 16:46:27 +0530

    Only first component of list is visible

--- MainActivity.java
+++ MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -22,11 +23,15 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity
 {
+    ListView listView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView=findViewById(R.id.listView);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Api.BASE_URL)
@@ -38,17 +43,29 @@ public class MainActivity extends AppCompatActivity
 
         call.enqueue(new Callback<List<Astute>>() {
             @Override
-            public void onResponse(Call<List<Astute>> call, Response<List<Astute>> response) {
+            public void onResponse(Call<List<Astute>> call, Response<List<Astute>> response)
+            {
 
                 List<Astute> quelist = response.body(); //gives question list
 
-                for(Astute a:quelist)
+
+                /*for(Astute a:quelist)
                 {
                     Log.d("userId",a.getUserId());
                     Log.d("id",a.getId());
                     Log.d("title",a.getTitle());
                     Log.d("body",a.getBody());
+                }*/
+
+                String[] questions = new String[quelist.size()]; //creating a array of string for list view
+                for (int i = 0; i < quelist.size(); i++)
+                {
+                    questions[i] = quelist.get(i).getId();
                 }
+
+
+                //displaying the string array into listview
+                listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, questions));
             }
 
             @Override
